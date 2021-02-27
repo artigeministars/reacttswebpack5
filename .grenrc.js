ba8f1068218749d012dee4aee82324dd34fb6ba9
artigeministars <@Bitbucket@>
2021-02-28 02:57:53 +0300

    initial commit

---
+++ .grenrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "dataSource": "commits",
+    "prefix": "gemini",
+    "includeMessages": "all",
+    "ignoreCommitsWith": ["geminiignore", "grenignore", "geminiskip", "grenskip"],
+    "ignoreTagsWith": ["geminiignoretag", "grenignoretag", "geminiskiptag", "grenskiptag"],
+    "changelogFilename": "CHANGELOG.md",
+}
---
+++ config/webpack/webpack.dev.config.ts
@@ -0,0 +1,90 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+
+
+
+const config: webpack.Configuration = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+            'style-loader',
+            {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1, modules: true }},
+            {loader: 'postcss-loader', options: {sourceMap: true}},
+            {loader: 'sass-loader', options: {sourceMap: true}},
+        ],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              eslintPath: require.resolve('eslint'),
+    
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      filename: 'index.html', // output file
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+        async: false
+    }),
+    new ESLintPlugin({
+        extensions: ["js", "jsx", "ts", "tsx"],
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    historyApiFallback: true,
+    port: 4000,
+    open: true,
+    hot: true
+  },
+};
+
+export default config;
