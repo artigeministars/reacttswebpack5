33640ef172b06cb71d6b5e2e7f08211ddb45a3fc
artigeministars <@Bitbucket@>
2021-02-28 03:20:24 +0300

    build & ignore files arranges

--- .grenrc.js
+++ .grenrc.js
@@ -5,4 +5,14 @@ module.exports = {
     "ignoreCommitsWith": ["geminiignore", "grenignore", "geminiskip", "grenskip"],
     "ignoreTagsWith": ["geminiignoretag", "grenignoretag", "geminiskiptag", "grenskiptag"],
     "changelogFilename": "CHANGELOG.md",
+    "template": {
+        commit: ({ message, url, author, name }) => `- [${message}](${url}) - ${author ? `@${author}` : name}`,
+        issue: "- {{labels}} {{name}} [{{text}}]({{url}})",
+        label: "[**{{label}}**]",
+        noLabel: "closed",
+        group: "\n#### {{heading}}\n",
+        changelogTitle: "# Changelog\n\n",
+        release: "## {{release}} ({{date}})\n{{body}}",
+        releaseSeparator: "\n---\n\n"
+    }
 }
